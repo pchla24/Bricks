084dfa09ddbad252f39ea660386c0326de5a0446
pchla24 <janusz_sawicki97@o2.pl>
2017-12-04 23:20:10 +0100

    pionowe ruchy

--- Bricks/src/bricks/Node.java
+++ Bricks/src/bricks/Node.java
@@ -32,6 +32,27 @@ public class Node {
 					children.add(node);
 				}
 		
+		for(int y=0; y<board.size-1; y++)						// vertical moves
+			for(int x=0; x<board.size; x++)
+				if(board.isLegalMove(x, y, x, y+1)){
+					Node node = new Node();
+					node.moveX1 = x;
+					node.moveY1 = y;
+					node.moveX2 = x;
+					node.moveY2 = y+1;
+					node.board.intBoard = board.intBoard;
+					if(whoseMove == 1) {
+						node.board.intBoard[x][y] = 1;
+						node.board.intBoard[x][y+1] = 1;
+						node.whoseMove = 2;
+					} else {
+						node.board.intBoard[x][y] = 2;
+						node.board.intBoard[x][y+1] = 2;
+						node.whoseMove = 1;
+					}
+					children.add(node);
+				}
+		
 		
 	}
 
