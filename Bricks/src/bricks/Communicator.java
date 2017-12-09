c23c8143e7452f1253c5f0bb1f3e38941567530a
pchla24 <janusz_sawicki97@o2.pl>
2017-12-09 19:09:41 +0100

    Metody komunikatora (do zrobienia)

--- Bricks/src/bricks/Communicator.java
+++ Bricks/src/bricks/Communicator.java
@@ -1,8 +1,12 @@
 package bricks;
 
+import java.util.ArrayList;
+
 public class Communicator {
 	
-	public int oppMoveX1, oppMoveY1, oppMoveX2, oppMoveY2, size;
+	public int oppMoveX1, oppMoveY1, oppMoveX2, oppMoveY2;
+	public int size;
+	ArrayList<Integer> obstacles = new ArrayList<>();
 	
 	public void readInit(String str) {
 		//TODO
